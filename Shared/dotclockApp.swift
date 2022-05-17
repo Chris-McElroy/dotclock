597aa33f16d106fd098bda7685b17ecb77bcad66
ChrisMcElroy <csmcelroy@me.com>
2022-05-17 13:04:49 -0700

    Initial Commit

---
+++ Shared/dotclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  dotclockApp.swift
+//  Shared
+//
+//  Created by Chris McElroy on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct dotclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
